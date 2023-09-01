ff8a8d6cbc05c5d8d802f8e96ab908d220f4b64c
Olga <workpost.mkh@yandex.ru>
2023-09-01 15:04:15 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// напишите прошграмму замены эелементов массива. 
+//Положительные элементы замените на соответствующие отрицательные и наоборот
+
+void ChangeSign(int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] *= -1;
+}
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       Console.Write(array[i]); 
+       if (i < array.Length - 1) Console.Write(", ");
+    }   
+}
+
+int[] itsArray = {-1, -6, 10, -17, 28, 8, 0, 7};
+ChangeSign(itsArray);
+PrintArray(itsArray);
+
+
+
