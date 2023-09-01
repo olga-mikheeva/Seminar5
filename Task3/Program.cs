5dcce978df2b0451d6dfef188c59c51c2de57e57
Olga <workpost.mkh@yandex.ru>
2023-09-01 15:46:41 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Создайте массив. Напишите программу которая определяет присутстует ли заданное число в массиве
+
+void FindNum(int[] fNum, int num)
+{
+int count = 0;
+for (int i = 0; i < fNum.Length; i++)
+{
+    if (fNum[i] == num)
+    {
+        count++;
+    }
+}
+if (count == 0)
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("да");
+}
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       Console.Write(array[i]); 
+       if (i < array.Length - 1) Console.Write(", ");
+    } 
+    Console.WriteLine();
+}
+
+
+int[] itsArray = {-6, 7, 19, 345, 3};
+PrintArray(itsArray);
+FindNum(itsArray, 7);
+
+
